0ba175ed5395558cb86fb384615619478b52116d
Mike Wess <mkwesselhoff@gmail.com>
2019-02-14 08:58:01 +0100

    deleted secret key and set DEBUG to False

--- blankapp/settings.py
+++ blankapp/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1#v)!5c5i!==!o9-pgz9&j0)l2*7u=qhf$3nd)ffo+t!53*nk5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
